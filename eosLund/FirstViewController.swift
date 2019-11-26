68a3f2ac08a8ad70da72d25ae867f80e04bd943c
Denis Rakitin <piggest@mail.ru>
2019-11-26 14:14:23 +0100

    Initial Commit

---
+++ eosLund/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  eosLund
+//
+//  Created by Denis Rakitin on 2019-11-26.
+//  Copyright © 2019 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
