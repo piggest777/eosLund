7ec60fc65e65b5e96c77cefbe486fe591bf8bc63
Denis Rakitin <piggest@mail.ru>
2019-11-30 02:56:08 +0100

    added next game countdown timer and information about next game

--- eosLund/Controller/GameScheduleVC.swift
+++ eosLund/Controller/GameScheduleVC.swift
@@ -22,6 +22,8 @@ class GameScheduleVC: UIViewController {
     @IBOutlet weak var leftDaysToGameLbl: UILabel!
     @IBOutlet weak var leftHoursToGameLbl: UILabel!
     @IBOutlet weak var leftMintoGameLbl: UILabel!
+    @IBOutlet weak var daysGameLbl: UILabel!
+    @IBOutlet weak var hoursGameLbl: UILabel!
     @IBOutlet weak var scheduleTableView: UITableView!
     @IBOutlet weak var viewHidhConstraint: NSLayoutConstraint!
     @IBOutlet weak var fullScheduleBtn: UIButton!
@@ -30,6 +32,8 @@ class GameScheduleVC: UIViewController {
     private lazy var gamesReference:CollectionReference = Firestore.firestore().collection(GAMES_REF)
     private var gameScheduleListener: ListenerRegistration!
     private var isMenTeam: Bool = true
+    var timer = Timer()
+    var timeIntervalToNextGame: Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,13 +49,14 @@ class GameScheduleVC: UIViewController {
         if gameScheduleListener != nil {
             gameScheduleListener.remove()
         }
+        timer.invalidate()
     }
     
     
     func setListener () {
         gameScheduleListener = gamesReference
         .whereField(IS_MEN_TEAM, isEqualTo: isMenTeam)
-        .order(by: GAME_DATE_AND_TIME, descending: true)
+        .order(by: GAME_DATE_AND_TIME, descending: false)
         .addSnapshotListener({ (snapshot, error) in
             if let error = error {
                 debugPrint("error fetching docs: \(error)")
@@ -59,8 +64,83 @@ class GameScheduleVC: UIViewController {
                 self.gamesArray.removeAll()
                 self.gamesArray = Game.parseData(snapshot: snapshot)
                 self.scheduleTableView.reloadData()
+                self.loadInfoToNextGameView()
             }
             })
+    }
+    
+    func loadInfoToNextGameView() {
+//    gamesArray.sort { (game1, game2) -> Bool in
+//        if game1.gameDateAndTime > game2.gameDateAndTime {
+//            return true
+//        } else {
+//            return false
+//            }
+//        }
+        
+//        need to add function to hide nextgame section if no games on future or change next game information
+        guard gamesArray.count != 0 else { return }
+        let filteredGamesArray = gamesArray.filter { (game) -> Bool in
+            let currentDate = Date()
+            if game.gameDateAndTime > currentDate {
+                return true
+            } else {
+                return false
+            }
+        }
+        
+        let nextGame: Game = filteredGamesArray.first!
+        
+        firstTeamNameLbl.text = nextGame.team1Name
+        secondTeamNameLbl.text = nextGame.team2Name
+        nextGamePlaceLbl.text = "\(nextGame.gameCity!), \(nextGame.gamePlace!)"
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "d MMM yyyy, HH:mm"
+        formatter.locale = .current
+        formatter.timeZone = TimeZone(identifier: "Europe/Stockholm")
+        
+        nextGameDateAndTimeLbl.text = formatter.string(from: nextGame.gameDateAndTime)
+        let currentDate = Date()
+        timeIntervalToNextGame = nextGame.gameDateAndTime.timeIntervalSince(currentDate)
+        updateTimerLbl(timeInterval: timeIntervalToNextGame)
+        timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if timeIntervalToNextGame > 0.0 {
+            timeIntervalToNextGame -= 60.0
+            updateTimerLbl(timeInterval: timeIntervalToNextGame)
+        } else {
+            timer.invalidate()
+        }
+    }
+    
+    func updateTimerLbl(timeInterval: Double) {
+        if timeInterval > 0.0 {
+            let days = Int(timeIntervalToNextGame / (24 * 3600))
+            let hours = Int(timeIntervalToNextGame.truncatingRemainder(dividingBy: 86400) / 3600)
+            let minutes = Int(timeIntervalToNextGame) / 60 % 60
+            
+            leftDaysToGameLbl.text = String(days)
+            leftHoursToGameLbl.text = String(hours)
+            leftMintoGameLbl.text = String(minutes)
+            if days <= 1 {
+                daysGameLbl.text = "Day"
+            } else {
+                daysGameLbl.text = "Days"
+            }
+            
+            if hours <= 1 {
+                hoursGameLbl.text = "Hour"
+            } else {
+                hoursGameLbl.text = "Hours"
+            }
+        } else {
+            leftDaysToGameLbl.text = "00"
+            leftHoursToGameLbl.text = "00"
+            leftMintoGameLbl.text = "00"
+        }
     }
     
 
@@ -72,6 +152,18 @@ class GameScheduleVC: UIViewController {
     }
     
     @IBAction func segmentControlWasSwitched(_ sender: Any) {
+        switch manOrWomanSegmentControl.selectedSegmentIndex {
+        case 0:
+            isMenTeam = true
+        case 1:
+            isMenTeam = false
+        default:
+             isMenTeam = true
+        }
+        
+        gameScheduleListener.remove()
+        timer.invalidate()
+        setListener()
     }
       
 }
